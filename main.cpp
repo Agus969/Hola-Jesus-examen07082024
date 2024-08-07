1fdbf9d4ea7df1639a50a861580bb4b44bf1d2db
Agus969 <agus.tin.2024@outlook.com>
2024-08-07 11:29:23 +0200

    se han modificado con una funcion

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
+//declaracion de funciones
+void funcion1(){
+    cout<<"Hola Jesus"<<endl;
 
-
+}
 int main(){
+    //llamada a las funciones
+    funcion1():
+    //mensaje de salida 
     cout<<"Hola Jesus";
 
     return 0;
